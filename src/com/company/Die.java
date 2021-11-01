4555abe8d1bf2642e098689f019ed8b97693c1a3
kirtanpatel <kirtanpatel@billericak12.com>
2021-11-01 12:04:43 -0400

    vddbsgfsbb

---
+++ src/com/company/Die.java
@@ -0,0 +1,9 @@
+//package com.company;
+//
+//public class Die {
+//    int totalDots=0;
+//    int die1 = (int)(1+Math.random()*6);
+//    int die2 =  (int)(1+Math.random()*6);
+//    totalDots = die1+die2;
+//        return totalDots;
+//}
